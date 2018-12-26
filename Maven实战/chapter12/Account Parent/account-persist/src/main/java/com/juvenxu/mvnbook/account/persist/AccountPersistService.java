7ef1fe9b857c56313b7e2870b3c508e3f783f45b
Loops <30333874+MrShawnYoung@users.noreply.github.com>
2018-12-26 20:50:16 +0800

    实例代码
    实例代码

---
+++ Maven实战/chapter12/Account Parent/account-persist/src/main/java/com/juvenxu/mvnbook/account/persist/AccountPersistService.java
@@ -0,0 +1,11 @@
+package com.juvenxu.mvnbook.account.persist;
+
+public interface AccountPersistService {
+	Account createAccount(Account account) throws AccountPersistException;
+
+	Account readAccount(String id) throws AccountPersistException;
+
+	Account updateAccount(Account account) throws AccountPersistException;
+
+	void deleteAccount(String id) throws AccountPersistException;
+}
